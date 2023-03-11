5100ec0c10112af752aa346deda5ea65551cda4d
ksamorodov <ksamorodov@yandex.ru>
2023-03-11 22:41:34 +0300

    Implement PublishSubscribe pattern with project reactor

--- project-react-publich-subscribe/src/main/java/com/ksamorodov/task/saber/Application.java
+++ project-react-publich-subscribe/src/main/java/com/ksamorodov/task/saber/Application.java
@@ -1,7 +1,47 @@
 package com.ksamorodov.task.saber;
 
-public class Application {
-    public static void main(String[] args) {
+import com.ksamorodov.task.saber.demo.Demonstration;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.FluxSink;
 
+public class Application {
+    /**
+     * Метод, запускающий демонстрацию работы паттерна Publish-Subscribe.
+     *
+     * @param args аргументы командной строки, задающие настройки демонстрации
+     *             в следующем порядке: стратегия паблишера (например, "IGNORE", "ERROR", DROP)
+     */
+    public static void main(String[] args) {
+        FluxSink.OverflowStrategy strategyKind = null;
+
+        // Проверяем количество аргументов командной строки и устанавливаем значение strategyKind и batchSize в соответствии с этим
+        if (args.length == 1) {
+            strategyKind = FluxSink.OverflowStrategy.valueOf(args[0].toUpperCase()); // Приводим значение аргумента к верхнему регистру для более надежного сравнения
+        } else {
+            // Если количество аргументов не соответствует ожидаемому, выводим сообщение об ошибке и завершаем программу
+            System.err.println("Usage: java Application [strategy]");
+            System.exit(1);
+        }
+
+        // Запускаем демонстрацию с заданными параметрами
+        Demonstration.start(strategyKind);
+//
+//            Flux<String> flux = Flux.create(emitter -> {
+//                emitter.next("Hello");
+//                emitter.next("World");
+//                emitter.complete();
+//            });
+//
+//            flux.subscribe(System.out::println);
+//
+//        Flux<String> sink = Flux.create(emitter -> {
+//                emitter.next("This");
+//                emitter.next("is");
+//                emitter.next("a");
+//                emitter.next("test");
+//                emitter.complete();
+//        });
+//
+//        sink.subscribe(System.out::println);
     }
 }
